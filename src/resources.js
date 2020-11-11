b8e0ebd6adcf6eff874c703a833197d5ae5231e7
Gio <sethg@ipi.org>
2020-11-10 22:24:27 -0600

    More resource resolution logic

--- src/resources.js
+++ src/resources.js
@@ -2,6 +2,24 @@
 
 var assets_root
 
+function fileIsAsset(url){
+    // Given a url, *without considering the domain*, determine if this should
+    // route to an asset of some sort. 
+
+    // If this function is false, url is either an internal vue link
+    // or an external web page.
+
+    // ...except for these files that are part of the installer.
+    let is_bundled = /\/assets\/[^/]+\.[^/]+/.test(url)
+    if (is_bundled) return false
+
+    let has_file_ext = /\.(jpg|png|gif|swf|txt|mp3|wav|mp4|webm)$/i.test(url)
+
+    // if you reference an html file in `archive/` that should match too, as a failsafe
+
+    return has_file_ext // ||  /^archive\//i.test(url) // maybe not needed now?
+}
+
 function resolveURL(url) {
     // TODO URL handling
     let resource_url = getResourceURL(url)
@@ -22,27 +40,44 @@ function getResourceURL(request_url){
     // Things this might get:
     // *.mspaintadventures
     // arbitrary HTTP urls
-    // http://localhost urls
+    // http://localhost:8080/ or app:// urls
+    // http://localhost:{port}/ urls
     //
     // Things this should not get:
     // assets://
 
+    // coerce logical vue resources to app://
+    // // do not actually
     let resource_url = request_url
+    //     .replace(/^http(s{0,1}):\/\/127\.0\.0\.1:8080\//, "app://") 
+    //     .replace(/^http(s{0,1}):\/\/localhost:8080\//, "app://") 
+
+    // Preliminary filtering
+    resource_url = resource_url
         .replace(/.*mspaintadventures.com(\/credits\/(?:sound|art)credits)/, "$1") //Linked from a few flashes
         .replace(/.*mspaintadventures.com\/((scratch|trickster|ACT6ACT5ACT1x2COMBO|ACT6ACT6)\.php)?\?s=(\w*)&p=(\w*)/, "/mspa/$4") //Covers for 99% of flashes that link to other pages
         .replace(/.*mspaintadventures.com\/\?s=(\w*)/, "/mspa/$1") //Covers for story links without page numbers
         .replace(/.*mspaintadventures.com\/extras\/PS_titlescreen\//, "/unlock/PS_titlescreen") //Link from CD rack flash
-        
+        .replace(/^http(s{0,1}):\/\/((www|cdn)\.)?mspaintadventures\.com/, "assets://") //Complete, should ideally never happen and probably won't work properly if it does
+    
         .replace(/^http(s{0,1}):\/\/www\.sweetcred\.com/, `assets://archive/sweetcred`)
         .replace(/^http(s{0,1}):\/\/www\.timelesschaos\.com\/transferFiles/, `assets://storyfiles/hs2/03318` ) // return to core - 618heircut.mp3
         .replace(/(www\.turner\.com\/planet\/mp3|fozzy42\.com\/SoundClips\/Themes\/Movies|pasko\.webs\.com\/foreign)/, `assets://storyfiles/hs2/00338`) // phat beat machine
-        
-        // .replace(/^http(s{0,1}):\/\/localhost:[0-9]+\//, "assets://") // same as app://
-        .replace(/^\//, "assets://")
-        .replace(/^http(s{0,1}):\/\/127\.0\.0\.1:[0-9]+\//, "assets://")
-
-        .replace(/^http(s{0,1}):\/\/((www|cdn)\.)?mspaintadventures\.com/, "assets://") //Complete, should ideally never happen and probably won't work properly if it does
     
+
+    // If it has a file extension, it should be an asset://
+    // If it does not, it is a logical vue page.
+    if (fileIsAsset(resource_url)) {
+        resource_url = resource_url
+            .replace(/^\//, "assets://")
+            .replace(/^http(s{0,1}):\/\/127\.0\.0\.1:[0-9]+\//, "assets://")
+            .replace(/^http(s{0,1}):\/\/localhost:[0-9]+\//, "assets://")  // if this accidently catches localhost:8080 we're boned
+
+        if (!/\.(jpg|png|gif|swf|txt|mp3|wav|mp4|webm)$/i.test(resource_url))
+            // files like 'archive/xxx'
+            resource_url = "assets://" + resource_url
+    }
+
     if (resource_url != request_url) {
         console.log("[Resource]", "[getResU]", request_url, "to", resource_url)
     } else {
