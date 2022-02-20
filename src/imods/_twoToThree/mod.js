b14ae8c460f7f31e391a283e00f4aaf80832d317
Gio <sethg@ipi.org>
2022-02-19 22:30:07 -0600

    Fix missing scarecrowking fanart

--- src/imods/_twoToThree/mod.js
+++ src/imods/_twoToThree/mod.js
@@ -7,6 +7,8 @@ module.exports = {
   },
   routes: {
     'assets://storyfiles/hs2/03085/03085_hq.swf': './swf/03085_hq.swf',
-    'assets://storyfiles/hs2/04082/04082_hq.swf': './swf/04082_hq.swf'
+    'assets://storyfiles/hs2/04082/04082_hq.swf': './swf/04082_hq.swf',
+    'assets://newspost_images/scarecrowking1.gif': 'assets://archive/social/news/scarecrowking1.gif',
+    'assets://newspost_images/scarecrowking2.gif': 'assets://archive/social/news/scarecrowking2.gif',
   }
 }
