e1ca82c7b991c769efa9109dc9c9d52161ee4320
Gio <sethg@ipi.org>
2022-03-03 21:30:45 -0600

    Fix #344, fix #339

--- src/imods/_twoToThree/mod.js
+++ src/imods/_twoToThree/mod.js
@@ -10,5 +10,9 @@ module.exports = {
     'assets://storyfiles/hs2/04082/04082_hq.swf': './swf/04082_hq.swf',
     'assets://newspost_images/scarecrowking1.gif': 'assets://archive/social/news/scarecrowking1.gif',
     'assets://newspost_images/scarecrowking2.gif': 'assets://archive/social/news/scarecrowking2.gif',
+  },
+  edit(archive) {
+    archive.music.albums['hiveswap-act-2-ost'].art = [] // Fix tobyfox crash
+    archive.music.tracks['flying-car'].bandcampId = 415056291 // Bowman - Fly to ES - Flying Car
   }
 }
