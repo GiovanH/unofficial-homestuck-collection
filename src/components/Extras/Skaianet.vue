b07cad721748639910f0a212e078029f9154d9eb
Gio <sethg@ipi.org>
2023-03-21 23:36:05 -0500

    Never use breaks, regret applying cssreset.

--- src/components/Extras/Skaianet.vue
+++ src/components/Extras/Skaianet.vue
@@ -36,12 +36,16 @@
                         The apology message reads:
                       </p>
                       <SpoilerBox kind="Apology">
-                        Originally there was some backstory content hidden in some cursed tags in this site code. I came up with the concepts years ago for internal purposes, not originally intended for public consumption. I thought some might find it interesting if the material were leaked in the site code in an obscure way. I think it has proven to be neither obscure as imagined, nor all that rewarding to read. The subject matter was inflammatory in careless ways, and the negative reactions to it are legitimate and should have been expected. A drafting process can be messy and can result in some ill-advised nonsense which often should not see the light of day. Letting a bunch of rough draft text files be deployed in this way was not a well considered move. Obviously this material has hurt a lot of people. Which isn't what I was going for, and didn't anticipate, but certainly should have. As far as the intentions behind it, I'm sure I believed it would be read as condemnation through satire, but instead it came off as excessively irreverent and disrespectful of a sensitive topic (to be clear, the main topic relates to antisemitism, which of course is not an issue that should be handled frivolously). After receiving criticism it's obvious to me where I'd been mistaken, and why no one could have possibly read it the way it was intended. I know this content was bad, I regret putting it in a place where people would discover it, and I regret that it was drafted in the first place.
-                        <br><br>
-                        I acknowledge that there are certain subjects which demand an exceptional level of care, and that I have been inexcusably thoughtless in handling this. I understand that there should be a certain standard of consideration and review for the content that is presented to an audience, and in forgoing that consideration there have been serious consequences for those people whom this issue directly affects. This could have been easily avoided had I sought a more careful review before it was posted to ensure that what I actually wanted to convey was being properly conveyed.
-                        <br><br>
-                        I am sorry that this lapse of judgment has been hurtful to so many people. I cannot undo the damage that this has caused, but I can promise that I am aware of the gravity of this mistake and that, going forward, I will be more mindful of the serious impact careless decisions have on those they hurt.
-                      </SpoilerBox>
+                        <p>
+                          Originally there was some backstory content hidden in some cursed tags in this site code. I came up with the concepts years ago for internal purposes, not originally intended for public consumption. I thought some might find it interesting if the material were leaked in the site code in an obscure way. I think it has proven to be neither obscure as imagined, nor all that rewarding to read. The subject matter was inflammatory in careless ways, and the negative reactions to it are legitimate and should have been expected. A drafting process can be messy and can result in some ill-advised nonsense which often should not see the light of day. Letting a bunch of rough draft text files be deployed in this way was not a well considered move. Obviously this material has hurt a lot of people. Which isn't what I was going for, and didn't anticipate, but certainly should have. As far as the intentions behind it, I'm sure I believed it would be read as condemnation through satire, but instead it came off as excessively irreverent and disrespectful of a sensitive topic (to be clear, the main topic relates to antisemitism, which of course is not an issue that should be handled frivolously). After receiving criticism it's obvious to me where I'd been mistaken, and why no one could have possibly read it the way it was intended. I know this content was bad, I regret putting it in a place where people would discover it, and I regret that it was drafted in the first place.
+                        </p>
+                        <p>
+                          I acknowledge that there are certain subjects which demand an exceptional level of care, and that I have been inexcusably thoughtless in handling this. I understand that there should be a certain standard of consideration and review for the content that is presented to an audience, and in forgoing that consideration there have been serious consequences for those people whom this issue directly affects. This could have been easily avoided had I sought a more careful review before it was posted to ensure that what I actually wanted to convey was being properly conveyed.
+                        </p>
+                        <p>
+                          I am sorry that this lapse of judgment has been hurtful to so many people. I cannot undo the damage that this has caused, but I can promise that I am aware of the gravity of this mistake and that, going forward, I will be more mindful of the serious impact careless decisions have on those they hurt.
+                      </p>
+                    </SpoilerBox>
                     </div>
                     <div class="centercolumn">
                       <SpoilerBox v-for="id in cursedId" :key="id" class="grey" :kind="id">
@@ -55,7 +59,7 @@
                     <table summary="" width="680px" border="2" bgcolor="">
                       <tr>
                         <td width="220px" bgcolor="#000">
-                          <center><MediaEmbed url="archive/skaianet/logob.png" class="log" size="30%" alt="" height="163" /></center>
+                          <center><MediaEmbed url="archive/skaianet/logob.png" class="log" size="30%" alt="" style="height: 163px;" /></center>
                         </td>
                         <td width="2"></td>
                         <td bgcolor="#000">
@@ -113,8 +117,8 @@
                     <br>
                     <table class="centercolumn promotion" summary="" border="2" v-for="promotion in promotions">
                       <tr>
-                        <td width="74px">
-                          <center><MediaEmbed :url="promotion.icon" /></center>
+                        <td width="74px" height="64">
+                          <MediaEmbed :url="promotion.icon" style="display: block;"/>
                         </td>
                         <td width="2"></td>
                         <td height="64">
@@ -125,7 +129,7 @@
                       </tr>
                       <tr>
                         <td colspan="3">
-                          <center style="padding:15px;" v-html="promotion.content" />
+                          <center style="padding: 0 15px;" v-html="promotion.content" />
                         </td>
                       </tr>
                     </table>
@@ -237,18 +241,18 @@ export default {
         {
           icon: "archive/skaianet/hs.gif",
           title: `
-            <font size="6"><a href="https://store.steampowered.com/app/623940/HIVESWAP_Act_1/" target="_blank">HIVESWAP.EXE</a></font>
+            <a href="https://store.steampowered.com/app/623940/HIVESWAP_Act_1/" target="_blank">HIVESWAP.EXE</a>
             <img src="assets://archive/skaianet/exe.gif" align="middle" />
-            <font size="6"><a href="http://www.hiveswap.com" target="_blank">WWW</a></font>
+            <a href="http://www.hiveswap.com" target="_blank">WWW</a>
             <img src="assets://archive/skaianet/www.gif" align="middle" />`,
           content: `
-            <p>Monsters in the yard. Monsters in the house. Monsters in the hall, beating on your door. Your brother's trapped in his treehouse, so he’s not gonna be much help. You're going to have to get yourself out of this. But where did they come from? How are you supposed to deal with this? Well, certainly not by cowering in your room. That's for, uh, cowards.</p><br>
+            <p>Monsters in the yard. Monsters in the house. Monsters in the hall, beating on your door. Your brother's trapped in his treehouse, so he’s not gonna be much help. You're going to have to get yourself out of this. But where did they come from? How are you supposed to deal with this? Well, certainly not by cowering in your room. That's for, uh, cowards.</p>
 
-            <p>First things first. You need to open the door.</p><br>
+            <p>First things first. You need to open the door.</p>
 
-            <p>HIVESWAP is a four-part series set in Andrew Hussie’s Homestuck universe, with a companion series, <em>Hauntswitch</em>, to follow. Continue your save from chapter to chapter, then from game to game, to carry your story, choices, friendships and frenemies across them all--then do it again! Once released, HIVESWAP and <em>Hauntswitch</em> will be playable in either order. Haven’t read Homestuck? That’s fine! This game is set in 1994! Homestuck hasn’t even been invented yet!</p><br>
+            <p>HIVESWAP is a four-part series set in Andrew Hussie’s Homestuck universe, with a companion series, <em>Hauntswitch</em>, to follow. Continue your save from chapter to chapter, then from game to game, to carry your story, choices, friendships and frenemies across them all--then do it again! Once released, HIVESWAP and <em>Hauntswitch</em> will be playable in either order. Haven’t read Homestuck? That’s fine! This game is set in 1994! Homestuck hasn’t even been invented yet!</p>
 
-            <p>Also see the <a href='http://hs.hiveswap.com/ezodiac/index.php'>Extended Zodiac</a>, an official personality quiz that assigns you a zodiac symbol based on your quiz answers and the Act 2 character preview <a href='http://hs.hiveswap.com/trollcall/'>Troll Call</a>, released as promotional material for the Hiveswap games.</p><br />
+            <p>Also see the <a href='http://hs.hiveswap.com/ezodiac/index.php'>Extended Zodiac</a>, an official personality quiz that assigns you a zodiac symbol based on your quiz answers and the Act 2 character preview <a href='http://hs.hiveswap.com/trollcall/'>Troll Call</a>, released as promotional material for the Hiveswap games.</p>
 
             <p>Hiveswap - <em>
               <a href="https://store.steampowered.com/app/623940/HIVESWAP_Act_1/">Act 1</a> |
@@ -268,23 +272,22 @@ export default {
         {
           icon: "archive/skaianet/viz.gif",
           title: `
-            <font size="6"><a href="https://www.homestuck.com/" target="_blank">HOMESTUCK.COM</a></font>
+            <a href="https://www.homestuck.com/" target="_blank">HOMESTUCK.COM</a>
             <img src="assets://archive/skaianet/www.gif" align="middle" />`,
           content: `
              <p>The current version of the MSPA website. More mobile-friendly, but with a number of issues. Hence this archive. Also has links to get official Homestuck merch.</p>
-             <br />
              <p><a href='/news/4-03-18'>Site Announcement</a></p>
              <p><em>2 April 2018 - Present</em></p>`
         },
         {
           icon: "archive/skaianet/fs.gif",
-          title: `<font size="6"><a href="https://store.steampowered.com/app/833040/Hiveswap_Friendsim/" target="_blank">FRIENDSIM.EXE</a></font>
-          <MediaEmbed url="archive/skaianet/exe.gif" align="middle" />
-          <font size="6"><a href="http://hs.hiveswap.com/friendsim/" target="_blank">WWW</a></font>
-          <MediaEmbed url="archive/skaianet/www.gif" align="middle" />`,
+          title: `<a href="https://store.steampowered.com/app/833040/Hiveswap_Friendsim/" target="_blank">FRIENDSIM.EXE</a>
+          <img src="assets://archive/skaianet/exe.gif" align="middle" />
+          <a href="http://hs.hiveswap.com/friendsim/" target="_blank">WWW</a>
+          <img src="assets://archive/skaianet/www.gif" align="middle" />`,
           content: `
-            <p>You just crash-landed on Alternia, and you’re <strong>DESPERATE</strong> for friendship! Anyone will do...wait, who are those two trolls approaching you?.</p><br>
-            <p>The Hiveswap Friendsim is a quick, loosely-canonical visual novel adventure following the efforts of the unnamed protagonist (that’s you!) to survive and maybe even thrive on the harsh surface of ALTERNIA. Set in the time of Hiveswap: Act 1, this episodic visual novel’s opening volume, written by Homestuck creator Andrew Hussie, is sure to satisfy Homestuck and Hiveswap fans who are eager for a darker stroll across the Alternian landscape.</p><br>
+            <p>You just crash-landed on Alternia, and you’re <strong>DESPERATE</strong> for friendship! Anyone will do...wait, who are those two trolls approaching you?.</p>
+            <p>The Hiveswap Friendsim is a quick, loosely-canonical visual novel adventure following the efforts of the unnamed protagonist (that’s you!) to survive and maybe even thrive on the harsh surface of ALTERNIA. Set in the time of Hiveswap: Act 1, this episodic visual novel’s opening volume, written by Homestuck creator Andrew Hussie, is sure to satisfy Homestuck and Hiveswap fans who are eager for a darker stroll across the Alternian landscape.</p>
 
             <p><em><a href="https://store.steampowered.com/app/833040/Hiveswap_Friendsim/">Description from Steam store page.</a></em></p>
             <p><em>13 April 2018 - 14 December 2018</em></p>`
@@ -292,14 +295,13 @@ export default {
         {
           icon: "archive/skaianet/epi.gif",
           title: `
-            <font size="6"><a href="https://www.homestuck.com/epilogues" target="_blank">EPILOGUES.TXT</a></font>
-            <MediaEmbed url="archive/skaianet/txt.gif" align="middle" />
-            <font size="6"><a href="https://www.viz.com/read/graphic-novel/homestuck-epilogues/product/6154" target="_blank">VIZ</a></font>
-            <MediaEmbed url="archive/skaianet/book.gif" align="middle" />`,
+            <a href="https://www.homestuck.com/epilogues" target="_blank">EPILOGUES.TXT</a>
+            <img src="assets://archive/skaianet/txt.gif" align="middle" />
+            <a href="https://www.viz.com/read/graphic-novel/homestuck-epilogues/product/6154" target="_blank">VIZ</a>
+            <img src="assets://archive/skaianet/book.gif" align="middle" />`,
           content: `<p>Tales of dubious authenticity.</p>
-            <br>
+
             <p>Ten years ago, a young man stood in his bedroom. The events set in motion that day would change his and his friends' lives forever, for the better and the worse (and the ridiculous). Now, in the aftermath, he has to make a choice: Meat or Candy?</p>
-            </br>
             <p><em><a href="https://www.viz.com/read/graphic-novel/homestuck-epilogues/product/6154">Description from VIZ Media.</a></em></p>
             <p><em>Online release: 13 - 20 April 2019</em></p>
             <p><em>VIZ Media print release: 14 January 2020</em></p>`
@@ -307,35 +309,33 @@ export default {
         {
           icon: "archive/skaianet/pq.gif",
           title: `
-            <font size="6"><a href="https://store.steampowered.com/app/1144030/Pesterquest/" target="_blank">PESTERQUEST.EXE</a></font>
-            <MediaEmbed url="archive/skaianet/exe.gif" align="middle" />
-            <font size="6"><a href="http://whatpumpkin.com/" target="_blank">WWW</a></font>
-            <MediaEmbed url="archive/skaianet/www.gif" align="middle" />`,
+            <a href="https://store.steampowered.com/app/1144030/Pesterquest/" target="_blank">PESTERQUEST.EXE</a>
+            <img src="assets://archive/skaianet/exe.gif" align="middle" />
+            <a href="http://whatpumpkin.com/" target="_blank">WWW</a>
+            <img src="assets://archive/skaianet/www.gif" align="middle" />`,
           content: `
             <p>Who are you again? And what the hell are you doing here? Wait… is this Earth, and who exactly is that kid over there? He looks nice! </p>
-            <br>
+
             <p>Embark on a quest of epic importance in Pesterquest, a grimsical episodic visual novel set in the darkly funny Homestuck/Hiveswap universe created by American author and artist <strong>Andrew Hussie.</strong></p>
-            <br>
+
             <p>Find all the answers you're looking for across 14 volumes of idiosyncratic adventure packed with visual mastery and shrewdly written narrative. As you zap between Earth and Alternia, you’ll discover and reconnect with up to 20 friends both extra and terrestrial.</p>
-            <br>
+
             <p><em><a href="https://store.steampowered.com/app/1144030/Pesterquest/">Description from Steam store page.</a></em></p>
             <p><em>4 September 2019 - 1 April 2020</em></p>`
         },
         {
           icon: "archive/skaianet/hs2.gif",
           title: `
-            <font size="6"><a href="https://www.homestuck2.com" target="_blank">HOMESTUCK^2.TXT</a></font>
-            <MediaEmbed url="archive/skaianet/txt.gif" align="middle" />`,
+            <a href="https://www.homestuck2.com" target="_blank">HOMESTUCK^2.TXT</a>
+            <img src="assets://archive/skaianet/txt.gif" align="middle" />`,
           content: `
             <p>This is not Homestuck. But it is not <em>not</em> Homestuck. This is...</p>
-            <br>
+
             <h2>HOMESTUCK^2: BEYOND CANON</h2>
-            <br>
+
             <p>Homestuck^2: Beyond Canon is an official continuation of the cult-classic webcomic Homestuck and a follow-up to The Homestuck Epilogues, dropped as a major event in April of 2019.</p>
             <p>Homestuck 2: Beyond Canon takes a combination of Andrew Hussie's original writing and plot outlines and concepts and joins him with a team of new, diverse voices to expand the compelling narratives of Homestuck's most (and least) beloved characters.</p>
-            </br>
             <p>On an indefinite hiatus as of Feb 2021. You can see the announcement post <a href='https://homestuck.net/official/patreon/posts/future-approach-47431875/'>here</a></p>
-            </br>
             <p><em><a href="https://www.patreon.com/homestuck">Description from the Homestuck Patreon.</a></em></p>
             <p><em>25 October 2019 - (HIATUS)</em></p>`
         }
@@ -438,16 +438,23 @@ export default {
     background-color: #b9b8b9;
     width: 685px;
   }
+}
 
-  table.promotion {
-    margin-bottom: 1em;
-    div.title {
-      width: 550px;
-      white-space: nowrap;
-      overflow: hidden;
-      font-size: 34px;
-      word-wrap: none;
-    }
+table.promotion::v-deep {
+  margin-bottom: 1em;
+  p {
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+  }
+  div.title {
+    width: 550px;
+    white-space: nowrap;
+    overflow: hidden;
+    font-size: xx-large;
+    word-wrap: none;
+
+    display: flex;
+    justify-content: center;
   }
 }
 
