3619923b80b45385baafe16f06aeccae580906cc
Gio <sethg@ipi.org>
2023-05-14 00:10:08 -0500

    Show more informational icons in mspfa index

--- src/components/CustomContent/MSPFAIndex.vue
+++ src/components/CustomContent/MSPFAIndex.vue
@@ -6,7 +6,12 @@
         <h2 class="pageTitle">MS Paint Fan Adventures</h2>
         <div class="adventureLinks">
           <div class="adventure" v-for="advlink in mspfaLinks" :key="advlink.href">
-            <a :href="advlink.href"><MediaEmbed :url="advlink.img" /><br /><span v-text="advlink.label" /></a>
+            <a :href="advlink.href" class="icon">
+              <MediaEmbed :url="advlink.img" />
+              <span v-text="advlink.label" />
+              <span class="date">{{ datestr(advlink) }}</span>
+              <span class="date">{{ advlink.pageCount }} pages</span>
+            </a>
           </div>
         </div>
       </div>
@@ -20,6 +25,8 @@ import NavBanner from '@/components/UIElements/NavBanner.vue'
 import PageFooter from '@/components/Page/PageFooter.vue'
 import MediaEmbed from '@/components/UIElements/MediaEmbed.vue'
 
+const DateTime = require('luxon').DateTime
+
 export default {
   name: 'MSPFALog',
   props: [
@@ -29,21 +36,34 @@ export default {
   },
   data: function() {
     return {
-      
     }
   },
   computed: {
     mspfaLinks(){
       return (Object.keys(this.$archive.mspfa) || []).map(k => {
+        const story = this.$archive.mspfa[k]
         return {
           href: `/mspfa/${k}/`,
-          img: this.$archive.mspfa[k].o || "assets://images/mspalogo_mspa.png",
-          label: this.$archive.mspfa[k].n
+          img: story.o || "assets://images/mspalogo_mspa.png",
+          label: story.n,
+          startDate: DateTime.fromMillis(story.d).setZone("America/New_York"),
+          updatedDate: DateTime.fromMillis(story.u).setZone("America/New_York"),
+          status: ['Inactive', 'Ongoing', 'Complete'][story.h-1],
+          pageCount: story.p.length
         }
       })
     }
   },
   methods: {
+    datestr(advlink){
+      const start_str = advlink.startDate.toFormat('LLL yyyy')
+      const updated_str = (advlink.status == 'Ongoing') ? '' : advlink.updatedDate.toFormat('LLL yyyy')
+      if (start_str == updated_str) {
+        return start_str
+      } else {
+        return `${start_str} - ${updated_str}`
+      }
+    }
   }
 }
 </script>
@@ -81,8 +101,22 @@ export default {
       .pageContent{
         background: var(--page-pageContent);
 
-        .pageContent img {
-            max-width: 100%;
+        a.icon {
+          text-align: center;
+          text-decoration: none;
+          > span {
+            display: block;
+            text-decoration: underline;
+          }
+
+          .date {
+            padding-top: 5px;
+            font-family: Verdana, Arial, Helvetica, sans-serif;
+            font-weight: normal;
+            color: var(--page-nav-meta);
+            font-size: 12px;
+            text-decoration: none;
+          }
         }
         
         width: 650px;     
@@ -111,6 +145,7 @@ export default {
               object-fit: contain;
               width: 164px;
               height: 164px;
+              display: inline-block;
             }
           }
         }
