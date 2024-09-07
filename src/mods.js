GiovanH/unofficial-homestuck-collection

commit 9aa2f71e16374e17354b957af1606593ec935998

Avoid readFileSync by using JS cache for mod store objects

src/mods.js
@@ -410,14 +410,35 @@ function buildApi(mod) {
     return fs.readFileSync(path.join(mod._mod_root_dir, local_path), 'utf8')
   }
 
+  var this_store_cache = {}
+  var cached_undefined = "_CACHED_UNDEFINED_"
+
   let api = {
     store: {
-      set: (k, v) => store_mods.set(getModStoreKey(mod._id, k), v),
-      get: (k, default_) => store_mods.get(getModStoreKey(mod._id, k), default_),
-      has: (k) => store_mods.has(getModStoreKey(mod._id, k)),
-      delete: (k) => store_mods.delete(getModStoreKey(mod._id, k)),
+      set: (k, v) => {
+        this_store_cache[k] = (v == undefined ? cached_undefined : v)
+        return store_mods.set(getModStoreKey(mod._id, k), v)
+      },
+      get: (k, default_) => {
+        if (this_store_cache[k] != undefined) {
+          if (this_store_cache[k] == cached_undefined) return undefined
+          else return this_store_cache[k]
+        } else {
+          const v = store_mods.get(getModStoreKey(mod._id, k), default_)
+          this_store_cache[k] = (v == undefined ? cached_undefined : v)
+          return v
+        }
+      },
+      has: (k) => this_store_cache[k] || store_mods.has(getModStoreKey(mod._id, k)),
+      delete: (k) => {
+        delete this_store_cache[k]
+        store_mods.delete(getModStoreKey(mod._id, k))
+      },
       onDidChange: (k, cb) => store_mods.onDidChange(getModStoreKey(mod._id, k), cb),
-      clear: () => store_mods.clear(getModStoreKey(mod._id, null))
+      clear: () => {
+        this_store_cache = {}
+        store_mods.clear(getModStoreKey(mod._id, null))
+      }
     }, 
     readFile(asset_path) {
       let data = readFileSyncLocal(asset_path, "readFile")

commit c8af220afdb02f5a82a2e79704331a5fdf6a4ebb

mods: fix storing `false`
`store.has` was broken in 9aa2f71e16374e17354b957af1606593ec935998,
such that a cached stored value of `false` is ignored.

`store.get` was broken in a64ec80332146e424dd7c218cb97ae055d4be4ac,
such that an uncached stored value of `false` is ignored.

src/mods.js
@@ -474,10 +474,10 @@ async function buildApi(mod) {
         } else {
           const v = store_mods.get(getModStoreKey(mod._id, k), default_)
           this_store_cache[k] = (v == undefined ? cached_undefined : v)
-          return v || default_
+          return v
         }
       },
-      has: (k) => this_store_cache[k] || store_mods.has(getModStoreKey(mod._id, k)),
+      has: (k) => this_store_cache[k] != undefined || store_mods.has(getModStoreKey(mod._id, k)),
       delete: (k) => {
         delete this_store_cache[k]
         store_mods.delete(getModStoreKey(mod._id, k))